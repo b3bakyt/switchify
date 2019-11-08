f1f9b70cea2cb0c7e43db552219f00ccd4e21e89
Bakyt B <b3bakyt@gmail.com>
2019-11-09 03:19:36 +0600

    in - should match value in array - test failed

---
+++ test/in.test.js
@@ -0,0 +1,17 @@
+const check = require('../src');
+const Chai  = require('chai');
+const {
+  expect,
+} = Chai;
+
+describe('in', function () {
+
+  it('should match value in array', function () {
+    const result = check(10)
+      .in([3, 10, 5])
+      .then(val => 'value matched!')
+      .else('not matched!');
+    expect(result).equals('value matched!');
+  });
+
+});
