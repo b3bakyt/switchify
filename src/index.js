617f83add15abbb3029c63a7dd1ebf22dd337732
Bakyt B <b3bakyt@gmail.com>
2019-11-09 02:09:47 +0600

    - first commit

---
+++ src/index.js
@@ -0,0 +1,63 @@
+
+const STATES = {
+  initial: 'initial',
+  condition_set: 'condition_set',
+  expression_set: 'expression_set',
+  else_set: 'else_set',
+};
+
+function getValue(data, val) {
+  if (typeof data === 'function')
+    return data(val);
+
+  return val;
+}
+
+function check(condition) {
+
+  const exp = {
+    matched: false,
+    value: undefined,
+    result: undefined,
+  };
+
+  function equals(val) {
+    if (condition = val) {
+      exp.matched = true;
+      exp.value = val;
+    }
+
+    return states[STATES.condition_set];
+  }
+  function then(expression) {
+    if (exp.matched)
+      exp.result = expression(exp.value);
+
+    return states[STATES.expression_set];
+  }
+  function els(val) {
+    if (!exp.matched)
+      return val;
+
+    return exp.result;
+  }
+
+  const states = {
+    [STATES.initial]: {
+      equals: equals,
+      else: els,
+    },
+    [STATES.condition_set]: {
+      then,
+    },
+    [STATES.expression_set]: {
+      equals,
+      else: els,
+    },
+    [STATES.else_set]: undefined,
+  };
+
+  return states[STATES.initial];
+}
+
+module.exports = check;
